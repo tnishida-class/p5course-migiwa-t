900d677bb6eae315c96c55a31fc6f92d60253599
migiwa-t <91937170+migiwa-t@users.noreply.github.com>
2021-10-12 17:29:34 +0900

    change_section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,17 @@
 // 小手調べ
 function setup(){
+  createCanvas(100, 100);
+  background(255);
+
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    if(i < 5){
+      stroke(0, 0, 255);
+    }
+    else{
+      stroke(255, 0, 0)
+    }
+    let x = i * 10 + 10;
+    ellipse(50, 50, x);
   }
 }

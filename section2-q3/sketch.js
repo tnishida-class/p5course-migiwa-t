6a70ec385974788693270b47fb1a9bbf7114d39d
migiwa-t <91937170+migiwa-t@users.noreply.github.com>
2021-10-13 21:24:39 +0900

    change_section2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -34,3 +34,54 @@ function drawArcs(c1, c2, r) {
     arc(cx, cy, r, r, start, stop, PIE);
   }
 }
+
+//上のやつよく分からないので自分でやってみる↓
+function setup(){
+  createCanvas(400, 400);
+  fill(0);
+  circle(200, 200, 400)
+
+  stroke(255);
+  strokeWeight(3);
+  for(let i = 0; i < 10; i++){
+    let x = i * 2
+    let y = i * 2 + 1
+    let z = i * 2 + 2
+    fill(0, 255, 0);//緑
+    arc(200, 200, 325, 325, PI / 10 * x, PI / 10 * y, PIE);
+    fill(255, 0, 0);//赤
+    arc(200, 200, 325, 325, PI / 10 * y, PI / 10 * z, PIE);
+  }
+  for(let i = 0; i < 10; i++){
+    let x = i * 2
+    let y = i * 2 + 1
+    let z = i * 2 + 2
+    fill(242, 212, 147);//ベージュ
+    arc(200, 200, 300, 300, PI / 10 * x, PI / 10 * y, PIE);
+    fill(0);//黒
+    arc(200, 200, 300, 300, PI / 10 * y, PI / 10 * z, PIE);
+  }
+  for(let i = 0; i < 10; i++){
+    let x = i * 2
+    let y = i * 2 + 1
+    let z = i * 2 + 2
+    fill(0, 255, 0);//緑
+    arc(200, 200, 200, 200, PI / 10 * x, PI / 10 * y, PIE);
+    fill(255, 0, 0);//赤
+    arc(200, 200, 200, 200, PI / 10 * y, PI / 10 * z, PIE);
+  }
+  for(let i = 0; i < 10; i++){
+    let x = i * 2
+    let y = i * 2 + 1
+    let z = i * 2 + 2
+    fill(242, 212, 147);//ベージュ
+    arc(200, 200, 175, 175, PI / 10 * x, PI / 10 * y, PIE);
+    fill(0);//黒
+    arc(200, 200, 175, 175, PI / 10 * y, PI / 10 * z, PIE);
+  }
+  fill(0, 255, 0);
+  circle(200, 200, 50);
+
+  fill(255, 0, 0);
+  circle(200, 200, 25);
+}

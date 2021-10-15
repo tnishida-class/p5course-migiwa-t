0ad39a23f30566a0708913c97aca0304e35440a7
migiwa-t <91937170+migiwa-t@users.noreply.github.com>
2021-10-15 10:03:50 +0900

    Update sketch.js

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -1,21 +1,65 @@
 // ギリシャ国旗
-function setup() {
-  const blue = color(0, 51, 160);
-  createCanvas(270, 180);
+//function setup() {
+  //const blue = color(0, 51, 160);
+  //createCanvas(270, 180);
   // noStroke();
+  //background(255);
+
+  //let d = height / 9; // 縞1本の太さ
+
+  //for(let i = 0; i < 9; i++){
+    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
+    //rect(0, i * d, width, (i + 1) * d);
+  //}
+
+  //fill(blue);
+  //let size = d * 5;
+  //rect(0, 0, size, size);
+
+  //fill(255);
+  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
+//}
+
+//アメリカ国旗作る
+function setup(){
+  createCanvas(380, 200);
   background(255);
 
-  let d = height / 9; // 縞1本の太さ
+  let d = height / 13;
 
-  for(let i = 0; i < 9; i++){
-    // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
-    rect(0, i * d, width, (i + 1) * d);
-  }
+  for(let i = 0; i < 13; i++){
+    if(i % 2 == 0){ //この中カッコ忘れずに！！
+    x = i;
+    noStroke(); //ストロークつけたくない時はこれ
+    fill(191, 13, 62); //星条旗の赤
+    rect(0, d * x, width, d);
+    }
+   }
 
-  fill(blue);
-  let size = d * 5;
-  rect(0, 0, size, size);
+   noStroke();
+   fill(0, 32, 91);//星条旗の青
+   rect(0, 0, 152, d * 7);
 
-  fill(255);
-  // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
-}
+   let e = 152 / 12
+   let f = d * 7 / 10
+
+   for(let i = 0; i < 11; i++){
+     for(let j = 0; j < 9; i++){
+       if((i % 2 == 1  && j % 2 == 1) || (i % 2 == 0 && i % 2 == 0)){
+       fill(255);
+       circle(e * j, f * i, 10);
+       }
+
+      // if(i % 2 == 0 && i % 2 == 0){
+        // fill(255);
+         //circle(e * j, f * i, 10)
+       }
+       //fill(255);
+       //circle();
+       }
+
+     }
+
+
+
+//}

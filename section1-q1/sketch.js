7be99d2c2723e3dabee581f55b282ba0f0c5f540
migiwa-t <91937170+migiwa-t@users.noreply.github.com>
2021-10-19 10:58:03 +0900

    section1-q1_change

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -2,6 +2,7 @@
 function setup(){
   createCanvas(200, 200);
   background(255);
+  stroke(22, 131, 46)
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
@@ -12,7 +13,7 @@ function setup(){
   noFill();
   strokeWeight(25);　//線を太くする
   strokeCap(SQUARE);　//切れ目を四角にする。これがないと丸になる。
-  arc(100 - 25, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
+  arc(100 - 27, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI * 5 + PI);
   //↑1番初めが円の中心のx座標?、QUARTER_PI * 数字 で開始の位置と終了の位置を指定
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます

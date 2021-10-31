743a13edb50a43f1e42b30459626858689f463d2
migiwa-t <91937170+migiwa-t@users.noreply.github.com>
2021-10-31 15:44:09 +0900

    Update sketch.js

--- section4-q2/sketch.js
+++ section4-q2/sketch.js
@@ -12,7 +12,32 @@ function setup(){
 
   // 円グラフを描くには割合が必要なので合計を計算しておく
   let total = 0;
-  for(let i = 0; i < scores.length; i++){ total += scores[i]; }
+  for(let i = 0; i < scores.length; i++){
+    total += scores[i];
+  　sum[i] += scores[i]
+  }
+
+  // let px, py
+  // px = i-1までの合計/total * (PI * 2) + (-PI / 2);
+  // py = iまでの合計/total * (PI * 2) + (-PI / 2);
+
+  // arc(200, 200, 380, 380, px, py, PIE)
+  // arc(200, 200, 380, 380, -PI / 2, (-PI / 2) + PI * 2 * scores[1] / total, PIE)
+  // arc(200, 200, 380, 380, (-PI / 2) + PI * 2 * scores[1] / total, (-PI / 2) + PI * 2 * (scores[1]+scores[2]) / total, PIE)
+
+
 
   // BLANK[1]
+
+
 }
+
+  // sum += scores[i];
+  //
+  //
+  // px = total
+  // py =
+  // arc(200, 200, 380, 380, px, , PIE);
+
+
+//-PI / 2, (-PI / 2) + (2 * PI) *

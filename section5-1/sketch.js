b7aa38031975f3192ac1316c02b15b8e98bf9d2d
migiwa-t <91937170+migiwa-t@users.noreply.github.com>
2021-11-13 18:55:41 +0900

    section5-1_change

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -1,14 +1,26 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
-  background(192);
+  createCanvas(270, 180);
+  background(0, 51, 153);
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
-    let y = 100 + sin(theta) * 50;
-    ellipse(x, y, 10);
+    let x = 135 + cos(theta) * 60;
+    let y = 90 + sin(theta) * 60;
+    star(x, y, 10);
   }
 }
 
+function star(cx, cy, r){
+  noStroke();
+  fill(255, 204, 0);
+  beginShape();
+  for(let i = 0; i < 5; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);//次に登場したやつ同士をくっつける
+  }
+  endShape(CLOSE);
+}
 // ヒント：section5-2 にある star 関数をここにコピーして、 draw 内で ellipse の代わりに使おう
